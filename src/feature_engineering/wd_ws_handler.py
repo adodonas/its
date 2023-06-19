371498f792b8bfa995273402ff41347c3bfc7810
Andrey Dodon <andrey.dodon@flolive.net>
2023-06-19 20:24:53 +0300

    added convert wind direction to radians

--- src/feature_engineering/wd_ws_handler.py
+++ src/feature_engineering/wd_ws_handler.py
@@ -1,4 +1,6 @@
 import os
+
+import numpy as np
 import pandas as pd
 import logging
 
@@ -8,35 +10,51 @@ logging.basicConfig(level=logging.INFO)
 def process_file(file_path, prefix):
     logging.info(f"Processing file {file_path}...")
 
-    # load csv file into a pandas DataFrame
+    # Load CSV file into a pandas DataFrame
     df = pd.read_csv(file_path)
 
-    # validate required columns
+    # Validate required columns
     required_columns = ['station', 'timestamp', 'rh', 'wd', 'ws']
     df.columns.values[0:5] = required_columns
     if not all(column in df.columns for column in required_columns):
         logging.error(f"File {file_path} is missing required columns.")
         return None
 
-    # convert 'timestamp' column to datetime
+    # Convert 'timestamp' column to datetime
     df['timestamp'] = pd.to_datetime(df['timestamp'], format='%d/%m/%Y %H:%M', errors='coerce')
 
-    # check if 'rh', 'wd', 'ws' are numeric and if not, convert them
+    # Check if 'rh', 'wd', 'ws' are numeric and if not, convert them
     for column in ['rh', 'wd', 'ws']:
         df[column] = pd.to_numeric(df[column], errors='coerce')
 
-    # drop rows with any invalid (NaN) values in 'rh', 'wd', 'ws' and 'timestamp' columns
+    # Drop rows with any invalid (NaN) values in 'rh', 'wd', 'ws' and 'timestamp' columns
     df.dropna(subset=['timestamp', 'rh', 'wd', 'ws'], inplace=True)
 
-    # separate numeric and non-numeric columns for resampling
-    df_numeric = df[['timestamp', 'rh', 'wd', 'ws']].set_index('timestamp')
-    df_non_numeric = df[['timestamp', 'station']].set_index('timestamp')
+    # Convert wind direction to radians
+    df['wd'] = np.deg2rad(df['wd'])
 
-    # resample the data to aggregate from 10 minutes to 1 hour
+    # Compute x and y components of the wind
+    df['x_component'] = df['ws'] * np.cos(df['wd'])
+    df['y_component'] = df['ws'] * np.sin(df['wd'])
+
+    # Set 'timestamp' as the index
+    df.set_index('timestamp', inplace=True)
+
+    # Separate numeric and non-numeric columns for resampling
+    df_numeric = df[['rh', 'x_component', 'y_component', 'ws']]
+    df_non_numeric = df[['station']]
+
+    # Resample the data to aggregate from 10 minutes to 1 hour
     df_numeric = df_numeric.resample('H').mean()
     df_non_numeric = df_non_numeric.resample('H').first()
 
-    # merge the resampled dataframes
+    # Calculate the average wind direction from the average x and y components
+    df_numeric['avg_wind_direction'] = np.rad2deg(np.arctan2(df_numeric['y_component'], df_numeric['x_component']))
+
+    # Handle the situation where the average direction is negative
+    df_numeric['avg_wind_direction'] = df_numeric['avg_wind_direction'].apply(lambda x: x + 360 if x < 0 else x)
+
+    # Merge the resampled dataframes
     df_resampled = pd.concat([df_non_numeric, df_numeric], axis=1).reset_index()
 
     df_resampled['area'] = prefix
