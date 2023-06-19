61a060acfc71312eb076a5a47fffd4081d72b785
Andrey Dodon <andrey.dodon@flolive.net>
2023-06-19 20:50:13 +0300

    calculate_and_save_summary updated

--- src/ml/lockdown_vs_holiday_NO_analysis.py
+++ src/ml/lockdown_vs_holiday_NO_analysis.py
@@ -32,9 +32,9 @@ def generate_hypothesis_text(p_value, p_value_threshold=0.05):
 
 
 def calculate_and_save_summary(df):
-    # Extract NO data for different conditions
-    lockdown_NO_data = df[df['lockdown'] == 1]['NO']
-    holiday_NO_data = df[df['jewish_holiday'] == 1]['NO']
+    # Exclude Jewish holidays during lockdown and vice versa
+    lockdown_NO_data = df[(df['lockdown'] == 1) & (df['jewish_holiday'] == 0)]['NO']
+    holiday_NO_data = df[(df['jewish_holiday'] == 1) & (df['lockdown'] == 0)]['NO']
 
     # Perform t-test
     t_statistic, p_value = calculate_ttest(lockdown_NO_data, holiday_NO_data)
@@ -46,53 +46,35 @@ def calculate_and_save_summary(df):
     conditions = ['Lockdown', 'Jewish Holiday']
     averages = [lockdown_NO_data.mean(), holiday_NO_data.mean()]
 
-    log_and_return(f"Mean NO levels during lockdowns and Jewish holidays: {dict(zip(conditions, averages))}")
+    log_and_return(f"Mean NO during: {dict(zip(conditions, averages))}")
 
     # Generate and save markdown file
     markdown_filename = "analysis.md"
     with open(markdown_filename, 'w') as file:
         file.write("# Analysis\n")
-        file.write(
-            "This report presents a statistical analysis comparing Nitric Oxide (NO) levels during lockdowns "
-            "and Jewish holidays.\n")
+        file.write("This report evaluates NO levels during lockdowns and Jewish holidays, separately.\n")
         file.write("\n")
         file.write("## Hypothesis\n")
         file.write(
-            "In scientific research, a hypothesis is a proposed explanation for a phenomenon. For this analysis, "
-            "the hypothesis is that the average NO levels are significantly different during lockdowns and Jewish "
+            "The hypothesis for this analysis is that the average NO levels differ between lockdowns and Jewish "
             "holidays.\n")
-        file.write("\n")
-        file.write(
-            "This is considered as the alternative hypothesis (H1). On the contrary, the null hypothesis (H0) is "
-            "that there is no significant difference in the average NO levels during lockdowns and Jewish holidays. T"
-            "he null hypothesis assumes that any kind of difference or importance you see in your data is due to "
-            "chance.\n")
-        file.write("\n")
         file.write(f"{hypothesis_text}\n")
         file.write("\n")
         file.write("## Mean NO levels\n")
-        file.write("The following are the average NO levels during the two different periods:\n")
-        file.write(f"During lockdown: {averages[0]}, During Jewish holidays: {averages[1]}\n")
+        file.write("The average NO levels during each period are:\n")
+        file.write(f"Lockdown: {averages[0]}, Jewish holidays: {averages[1]}\n")
         file.write("\n")
         file.write("## Conclusion\n")
         file.write(
-            f"The p-value was {p_value}. This p-value is used to determine the statistical significance of our "
-            f"results. The p-value is a measure of the probability that an observed difference could have occurred "
-            f"just by random chance.\n")
+            f"The p-value was {p_value}, measuring the likelihood that observed differences occurred by chance.\n")
         file.write("\n")
         file.write(
-            "A lower p-value suggests that the observed data are less likely to occur under the null hypothesis. "
-            "Therefore, a p-value less than or equal to 0.05 is often used as a cut-off for rejecting the null "
-            "hypothesis. In other words, if the p-value is less than or equal to 0.05, we consider the results "
-            "statistically significant, and we reject the null hypothesis in favor of the alternative hypothesis.\n")
+            "A p-value ≤ 0.05 is often used as a threshold for statistical significance, rejecting the null "
+            "hypothesis in favor of the alternative one.\n")
         file.write("\n")
         file.write(
-            "If the p-value is greater than 0.05, we consider the results not statistically significant, and we "
-            "do not reject the null hypothesis. This means that the observed difference in NO levels could have "
-            "occurred by chance, and there is no evidence to suggest that the NO levels are significantly different "
-            "during lockdowns and Jewish holidays.\n")
-        file.write("\n")
-        file.write("Interpretation of this result is left as an exercise for the reader.\n")
+            "A p-value > 0.05 indicates the observed difference in NO levels could be due to chance, suggesting "
+            "no significant difference between the periods.\n")
 
     logging.info(f"Markdown file saved as {markdown_filename}.")
     return markdown_filename
